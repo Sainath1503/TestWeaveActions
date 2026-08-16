5d70591fe6d3ab9181404e72fadc3945267aa2e0
Sainath1503 <58841677+Sainath1503@users.noreply.github.com>
2026-08-16 21:11:33 +0530

    Sync VeyraAI runner source

--- src/main/java/model/WebTestStep.java
+++ src/main/java/model/WebTestStep.java
@@ -2,9 +2,14 @@ package model;
 
 public class WebTestStep {
 
+    public String stepName;
     public String action;
     public String selector;
     public String value;
     public String note;
+    public String expectedVariableName;
+    public String expectedVariableValue;
+    public boolean expectedVariablePresent;
+    public int timeoutMs;
     public boolean suggested;
 }

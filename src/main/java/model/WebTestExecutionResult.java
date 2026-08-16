028c9a4e188d7dff59fa89d4dbc70d5092ec8b2d
Sainath1503 <58841677+Sainath1503@users.noreply.github.com>
2026-08-16 21:11:31 +0530

    Sync VeyraAI runner source

--- src/main/java/model/WebTestExecutionResult.java
+++ src/main/java/model/WebTestExecutionResult.java
@@ -2,12 +2,23 @@ package model;
 
 public class WebTestExecutionResult {
 
+    public String stepName;
     public String action;
     public String selector;
     public String expectedValue;
+    public String actualValue;
+    public String observedVariableName;
+    public String observedVariableValue;
     public String capturedVariableName;
     public String capturedVariableValue;
     public boolean passed;
     public String message;
     public long durationMs;
+    public String pageUrl;
+    public String pageTitle;
+    public String screenshotPath;
+    public String ariaSnapshot;
+    public String domSnapshot;
+    public String consoleMessages;
+    public String networkFailures;
 }
